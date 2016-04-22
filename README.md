3bc3233e77fe2afe4d89f47862fbf9b83fccf97a
pwhitter1 <pwhitter1@gwu.edu>
2016-04-22 19:54:32 -0400

    Updated README, put LCD sketch in a folder, and changed a few folder names

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Final-Project-Systems-Programming-Sense-It
 We created this game, which tests memory, speed, and dexterity using this code, various sensors, and an Arduino Uno.
+To play the game, you need to upload the FinalProject, Combined_Color_Accel, and Caitlin_to_Nathalie_Arduinos sketches on three separate computers (as well as set up the external sensors with the Arduinos properly).
+
---
+++ SD_Card_2/SD_Card_2.ino
@@ -0,0 +1,47 @@
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+void SDCard() {
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("Final Score: ");
+    myFile.print(score);
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
